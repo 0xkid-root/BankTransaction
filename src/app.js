78e6d9111366dab04a8f503c727e6a7648f9dc67
0xkid-root <prohackers698@gmail.com>
2026-02-17 22:11:34 +0530

    signup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 const express = require("express");
 const app = express();
+const authRouter = require("./routes/auth.routes");
+const cookieParser = require("cookie-parser");
+
+
+
+/**  express ke jo server hai by default body ke data ko read  nhi kar pta hai 
+ then hum user karte hai  express.json()
+
+*/
+app.use(express.json());    
+app.use(cookieParser());
+
+
+app.use("/api/v1/auth", authRouter);
 
 
 
